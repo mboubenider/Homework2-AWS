750a95c95b731d6ab6ef005b6baeb35b950430e9
madison@ac2.net <l1vyl1vy>
2020-02-28 13:19:02 -0600

    3rd commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,5 @@
 package sample;
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXListView;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,9 +7,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+
 
 import java.net.URL;
 import java.sql.*;
